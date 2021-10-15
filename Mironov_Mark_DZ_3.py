4e5d661a8f32d7abe0d1aaea4f7455aef57d2922
MarkMironov17 <91608019+MarkMironov17@users.noreply.github.com>
2021-10-15 10:51:53 +0300

    Решение третьей задачи

--- Mironov_Mark_DZ_3.py
+++ Mironov_Mark_DZ_3.py
@@ -1,15 +1,15 @@
 a = ("Процент")
 b = ("Процента")
 c = ("Процентов")
-i = [i for i in range(1,101)]
+n = [i for i in range(1,101)]
 numbs = {11,12,13,14}
-for i in range(100):
-    i = i + 1
-    if i in numbs:
-        print(i, "процентов")
-    elif i % 10 == 1:
-        print(i, "процент")
-    elif i % 10 > 1 and i % 10 <5:
-        print(i, "процента")
+for n in range(100):
+    n = n + 1
+    if n in numbs:
+        print(n, "процентов")
+    elif n % 10 == 1:
+        print(n, "процент")
+    elif n % 10 > 1 and n % 10 <5:
+        print(n, "процента")
     else:
-        print(i, "процентов")
+        print(n, "процентов")
